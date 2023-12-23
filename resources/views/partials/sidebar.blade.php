893989c22f7c8582551f6023bd6c132a563bf323
AndreMaulanaMustofa <andremm73@gmail.com>
2023-12-23 09:06:16 +0700

    Perbaikan sidebar yang sebelumnya tidak terlihat bagus

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,80 @@
+<aside class="main-sidebar sidebar-light elevation-4">
+    <!-- Brand Logo -->
+    <a href="" class="brand-link">
+        <img src="{{ asset('img/polinema_logo.png') }}" alt="Polinema Picture" class="rounded mx-auto d-block" height="100" width="100">
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="{{ asset('img/polinema_logo.png') }}" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Polinema</a>
+            </div>
+        </div>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-address-card"></i>
+                    <p>
+                        Data Mahasiswa
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fa-solid fa-folder"></i>
+                    <p>
+                        Data Kelas
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fa-solid fa-cloud"></i>
+                    <p>
+                        Data Absensi
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-solid fa-building"></i>
+                    <p>
+                        Akun Mahasiswa
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-solid fa-qrcode"></i>
+                    <p>
+                        QR Code
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-solid fa-gear"></i>
+                    <p>
+                        Pengaturan
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class=" nav-icon fas fa-solid fa-pen-to-square"></i>
+                    <p>
+                        Ubah Password
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+</aside>
