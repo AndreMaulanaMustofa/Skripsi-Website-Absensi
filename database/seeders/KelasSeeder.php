9a40db48ffa83ef6b58162fec37cd801d0a1bd49
AndreMaulanaMustofa <andremm73@gmail.com>
2023-12-26 14:03:36 +0700

    Membuat seeder baru dan memperbaiki seeder yang ada

---
+++ database/seeders/KelasSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\kelas;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KelasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        try {
+            DB::beginTransaction();
+
+            kelas::create([
+                'jumlah_mahasiswa' => 25,
+                'kelas' => 'TI-1D',
+                'jurusan' => 'Teknologi Informasi',
+            ]);
+            kelas::create([
+                'jumlah_mahasiswa' => 27,
+                'kelas' => 'TI-2D',
+                'jurusan' => 'Teknologi Informasi',
+            ]);
+            kelas::create([
+                'jumlah_mahasiswa' => 20,
+                'kelas' => 'TI-3D',
+                'jurusan' => 'Teknologi Informasi',
+            ]);
+            kelas::create([
+                'jumlah_mahasiswa' => 35,
+                'kelas' => 'TI-4D',
+                'jurusan' => 'Teknologi Informasi',
+            ]);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+            throw $th;
+        }
+    }
+}
