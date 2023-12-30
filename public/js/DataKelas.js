124aece5b18fc2d01550597e9932317db2be4f7a
AndreMaulanaMustofa <andremm73@gmail.com>
2023-12-30 15:28:26 +0700

    Penambahan edit dan delete script untuk kelas

--- public/js/DataKelas.js
+++ public/js/DataKelas.js
@@ -40,3 +40,72 @@ function validasiKelas(){
         });
     }
 }
+
+function editKelas() {
+    Swal.fire({
+        title: "Simpan Perubahan?",
+        icon: "question",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Ubah",
+        denyButtonText: `Batal`
+    }).then((result) => {
+        if (result.isConfirmed) {
+            var myForm = document.getElementById('FormKelas');
+
+            Swal.fire({
+                title: "Perubahan telah disimpan",
+                icon: "success",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            myForm.submit();
+
+        } else if (result.isDenied) {
+            Swal.fire({
+                title: "Perubahan telah dibatalkan",
+                icon: "error",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            window.location.href="/Kelas";
+        }
+    });
+}
+
+function deleteKelas(id){
+    Swal.fire({
+    title: "Hapus Data",
+    text: "Data yang dihapus tidak dapat dikembalikan.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Hapus",
+    cancelButtonText: "Batal"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
+
+            $.ajax({
+                type: "POST",
+                url: "/Kelas/deleteKelas/" + id,
+                data: {
+                    _token: csrfToken,
+                    _method: "DELETE",
+                },
+                success: function() {
+                    Swal.fire({
+                        title: "Terhapus!",
+                        text: "Data telah dihapus.",
+                        icon: "success",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    window.location.reload();
+                },
+            });
+        }
+    });
+}
