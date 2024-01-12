40c47a64723a1674bbfb523a93d92edbef388729
AndreMaulanaMustofa <andremm73@gmail.com>
2024-01-12 13:25:00 +0700

    menghubungkan function dengan file view

--- app/Http/Controllers/AkunController.php
+++ app/Http/Controllers/AkunController.php
@@ -6,9 +6,21 @@ use Illuminate\Http\Request;
 
 class AkunController extends Controller
 {
+    public function index(){
+        $title = "Pengaturan";
+
+        return view('pengaturan.index', compact('title'));
+    }
+
+    public function edit(){
+        $title = "Edit Pengaturan";
+
+        return view('pengaturan.edit', compact('title'));
+    }
+
     Public function indexPassword(){
         $title = "Ubah Password";
-        
+
         return view('pengaturan.password', compact('title'));
     }
 }
