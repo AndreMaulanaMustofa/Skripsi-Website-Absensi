6a19c3600de1499dcdb646853d6ca8276cc91525
AndreMaulanaMustofa <andremm73@gmail.com>
2024-02-01 12:16:28 +0700

    Penerapan Javascript Data Jurusan di view.blade.php

--- resources/views/layouts/view.blade.php
+++ resources/views/layouts/view.blade.php
@@ -38,6 +38,7 @@
     <script src="{{ asset('js/DataMahasiswa.js') }}"></script>
     <script src="{{ asset('js/DataKelas.js') }}"></script>
     <script src="{{ asset('js/KotaApi.js') }}"></script>
+    <script src="{{ asset('js/DataJurusan.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
 </head>
