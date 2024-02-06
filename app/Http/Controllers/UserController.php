0d6387668c31e6edf5e1a0ee9d61dde77a262165
AndreMaulanaMustofa <andremm73@gmail.com>
2024-02-06 09:46:14 +0700

    Penambahan update Password (belum fix)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -22,5 +25,33 @@ class UserController extends Controller
         $title = "Ubah Password";
 
         return view('pengaturan.password', compact('title'));
+    }
+
+    public function updatePassword(Request $request){
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        $passwordBaru = $request->input('newPass');
+        $passwordRepeat = $request->input('repNewPass');
+        $currentPass = $request->input('curPass');
+
+        if (Hash::check($currentPass, $user->password)) {
+            if ($passwordBaru == $passwordRepeat) {
+                $user->password = Hash::make($passwordBaru);
+                $user->update();
+
+                return back()->withErrors([
+                    'ubah-success' => 'success',
+                ]);
+            }else{
+                return back()->withErrors([
+                    'ubah-notSame' => 'error',
+                ]);
+            }
+        } else {
+            return back()->withErrors([
+                'ubah-fail' => 'Password saat ini salah!, Silahkan coba lagi.',
+            ]);
+        }
     }
 }
