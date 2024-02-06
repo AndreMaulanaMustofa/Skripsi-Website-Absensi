809eede3941a08873ac6bd6dbba3d8f614734ee4
AndreMaulanaMustofa <andremm73@gmail.com>
2024-02-06 09:45:46 +0700

    Penambahan variable mhs untuk memanggil data Mahasiswa

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\mahasiswa;
 
 class QRCodeController extends Controller
 {
     public function index(){
         $title = "QR Code Generator";
+        $mhs = mahasiswa::all();
 
-        return view('qrCode.qrcode', compact('title'));
+        return view('qrCode.qrcode', compact('title','mhs'));
     }
 }
