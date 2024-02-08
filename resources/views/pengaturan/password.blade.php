76b0555c36b8f512a67692e3a63ba211003216b1
AndreMaulanaMustofa <andremm73@gmail.com>
2024-02-08 17:40:14 +0700

    pembuatan ulang untuk semua notifkasi yang ada di password

--- resources/views/pengaturan/password.blade.php
+++ resources/views/pengaturan/password.blade.php
@@ -4,62 +4,91 @@
 <div class="content-wrapper" style="background-color: white; margin-top: 120px;">
     <div class="content-header layout">
         <div class="container-fluid">
-            <form action="#" method="POST">
-                @csrf
-                @method('PUT')
                 <div class="container mt-5" style="background-color: white; width: 60%; height: 100%;">
                     <br>
 
-                    @error('ubah-fail')
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                            <div class="message">
-                                <span class="text-red-500">{{$message = "Current Password Salah! Silahkan Coba Lagi."}}</span>
-                            </div>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @enderror
+                    <div class="container border" style="width: 90%; height: 80%; ">
+                        @error('ubah-fail')
+                            <script>
+                                const Toast = Swal.mixin({
+                                    toast: true,
+                                    position: "bottom-end",
+                                    showConfirmButton: false,
+                                    timer: 3000,
+                                    timerProgressBar: true,
+                                    didOpen: (toast) => {
+                                        toast.onmouseenter = Swal.stopTimer;
+                                        toast.onmouseleave = Swal.resumeTimer;
+                                    }
+                                });
+                                    Toast.fire({
+                                    icon: "error",
+                                    title: "Current Password tidak sesuai"
+                                });
+                            </script>
+                        @enderror
 
-                    @error('ubah-notSame')
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                            <div class="message">
-                                <span class="text-red-500">{{$message = "Repeat Password tidak sama dengan New Password."}}</span>
-                            </div>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @enderror
+                        @error('ubah-notSame')
+                            <script>
+                                const Toast = Swal.mixin({
+                                    toast: true,
+                                    position: "bottom-end",
+                                    showConfirmButton: false,
+                                    timer: 3000,
+                                    timerProgressBar: true,
+                                    didOpen: (toast) => {
+                                        toast.onmouseenter = Swal.stopTimer;
+                                        toast.onmouseleave = Swal.resumeTimer;
+                                    }
+                                });
+                                    Toast.fire({
+                                    icon: "error",
+                                    title: "Password baru tidak sama"
+                                });
+                            </script>
+                        @enderror
 
-                    @error('ubah-success')
-                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                            <div class="message">
-                                <span class="text-red-500">{{$message = "Password berhasil diubah!"}}</span>
-                            </div>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @enderror
-
-                    <form action="{{ route('admin.pass.update') }}" method="post">
-                        @csrf
-                        @method('put')
-                        <div class="container border" style="width: 90%; height: 80%; ">
+                        @error('ubah-success')
+                            <script>
+                                const Toast = Swal.mixin({
+                                    toast: true,
+                                    position: "bottom-end",
+                                    showConfirmButton: false,
+                                    timer: 3000,
+                                    timerProgressBar: true,
+                                    didOpen: (toast) => {
+                                        toast.onmouseenter = Swal.stopTimer;
+                                        toast.onmouseleave = Swal.resumeTimer;
+                                    }
+                                });
+                                    Toast.fire({
+                                    icon: "success",
+                                    title: "Password berhasil diubah"
+                                });
+                            </script>
+                        @enderror
                             <div class="row mt-3">
                                 <div class="row mb-2 mt-2">
                                     <h3>Ubah Password</h3>
                                 </div>
                             </div>
-                            <div class="row mt-4 ml-1">
-                                <div class="col-4 mt-2" >
-                                    Current Password
-                                </div>
-                                <div class="col-8 ">
-                                    <input type="password" id="curPass" name="curPass" class="form-control input-password" placeholder="Masukkan Password saat ini" required>
-                                </div>
+                        <form action="{{ route('pass.update', ['id' => $data->id]) }}" method="post">
+                            @csrf
+                            @method('PUT')
+                        <div class="row mt-4 ml-1">
+                            <div class="col-4 mt-2" >
+                                Current Password
                             </div>
+                            <div class="col-8">
+                                <input type="password" name="curPass" class="form-control input-password" placeholder="Masukkan Password saat ini" required>
+                            </div>
+                        </div>
                         <div class="row mt-4 ml-1">
                             <div class="col-4 mt-2">
                                 New Password
                             </div>
                             <div class="col-8">
-                                <input type="password" id="newPass" name="newPass" class="form-control input-password" placeholder="Masukkan Password Baru" required>
+                                <input type="password" name="newPass" class="form-control input-password" placeholder="Masukkan Password Baru" required>
                             </div>
                         </div>
                         <div class="row mt-4 ml-1">
@@ -67,7 +96,7 @@
                                 Repeat Password
                             </div>
                             <div class="col-8">
-                                <input type="password" id="repNewPass" name="repNewPass" class="form-control input-password" placeholder="Ulangi Masukkan Password Baru" required>
+                                <input type="password" name="repNewPass" class="form-control input-password" placeholder="Masukkan Ulang Password Baru" required>
                             </div>
                         </div>
                         <div class="row mt-3">
@@ -75,8 +104,8 @@
                             <div class="col-3">
                                 <button type="submit" style="width: 100%; margin-bottom: 13px; background-color:#42BB72; border:none; color:white; height: 40px">Simpan <i class="fa fa-save"></i></button>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                     </div>
                     <br>
                 </div>
