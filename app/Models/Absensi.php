0b7fe676de54a958e9c935252d3e27cc738af73b
AndreMaulanaMustofa <andremm73@gmail.com>
2024-02-11 21:20:39 +0700

    Pembuatan table Absensi

---
+++ app/Models/Absensi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Absensi extends Model
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'NIM',
+        'namaMahasiswa',
+        'kelas',
+        'jenisKelamin',
+        'semester',
+        'tgl_absen',
+        'jam_absen',
+        'status'
+    ];
+}
---
+++ database/seeders/AbsensiSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Absensi;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AbsensiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        try {
+            DB::beginTransaction();
+
+            Absensi::create([
+                'NIM' => '2041720211',
+                'namaMahasiswa' => 'Andre Maulana Mustofa',
+                'kelas' => 'TI - 3D',
+                'jenisKelamin' => 'Laki-laki',
+                'semester' => 5,
+                'tgl_absen' => now()->toDateString(),
+                'jam_absen' => '06.30',
+                'status' => 'Tidak Terlambat'
+            ]);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+            throw $th;
+        }
+    }
+}
